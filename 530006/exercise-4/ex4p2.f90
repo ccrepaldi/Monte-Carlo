6ad350222779ed01b89b9c14d7214abba12e8052
ccrepaldi <caike.crepaldi@gmail.com>
2016-02-25 02:36:12 +0200

    Problem 2 fixed -> part (a) done!

--- 530006/exercise-4/ex4p2.f90
+++ 530006/exercise-4/ex4p2.f90
@@ -1,12 +1,12 @@
 program main
   use mtmod
   implicit none
-  integer, parameter :: dp=kind(1.d0), Nf=38, Nc=100, Nd=5, Nh=10
+  integer, parameter :: dp=kind(1.d0), Nf=38, Nc=100, Nd=1000, Nh=10000
   character(len=80) :: filename
   integer :: i,j,k,ios
   real(dp), dimension(Nf) :: x,y
-  real(dp) :: xm_true,xmean,dxm,u,temp,Fk(Nf),means(Nd)
-  integer :: Dk(Nf),hj(Nh)
+  real(dp) :: xm_true,xmean,dxm,u,Fk(Nf),means(Nd),hj(Nh),Hk(Nh),xl,xu
+  integer :: Dk(Nf)
 
   ! Read the data ****************************************
 
@@ -31,22 +31,22 @@ program main
   !end do
 
   ! *******************************************************
+  
+  xm_true=0.d0
+  do i=1,Nf
+     xm_true=xm_true+x(i)*y(i)
+  end do
+  xm_true=xm_true/sum(y) ! get the true mean of the distribution
 
-  xm_true=sum(x)/38 ! get the true mean of the distribution
+  !print *, xm_true
 
   !Nc=100 ! number of counts in 1 distribution (declared as a parameter)
 
   !Nd=5 ! number of distributions (declared as a parameter)
 
-  !temp=0.d0
-
   y(:)=y(:)/sum(y) ! normalizes the distribution
 
   do k=1,Nf
-  !
-  !   temp=temp+(y(k)+y(k+1))/2*abs(x(k)-x(k+1))
-  !
-  !   Fk(k)=temp  ! get the normalized cumulative distribution
 
      Fk(k)=sum(y(1:k))  ! get the normalized cumulative distribution
 
@@ -56,10 +56,8 @@ program main
   !   print *, Fk(i)
   !end do
 
-  hj=0
-
-  dxm=100.d0
-  hj(:)=0
+  dxm=0.1d0
+  hj(:)=0.d0
   do i=1,Nd
      Dk(:)=0.d0
      do j=1,Nc
@@ -83,9 +81,34 @@ program main
      do j=1,Nh
         if (j==int(xmean/dxm)) hj(j)=hj(j)+1 ! doesnt make any sense
      end do
-
-     print *, hj
   end do
+  !print *, means
+  !print *, sum(hj), Nd ! should be equal, OK
+  !do k=1,Nh
+  !   print *, hj(k)
+  !end do
+  hj(:)=hj(:)/sum(hj) ! normalizes the distribution
+  do k=1,Nh
+     Hk(k)=sum(hj(1:k))  ! get the normalized cumulative distribution
+  end do
+  !do k=1,Nh
+  !   print *, Hk(k)
+  !end do
+
+  do k=1,Nh
+     if ((0.16.le.Hk(k)).and.(0.16.gt.Hk(k-1))) xl=dxm*k
+  end do
+
+  do k=1,Nh
+     if ((0.84.le.Hk(k)).and.(0.84.gt.Hk(k-1))) xu=dxm*k
+  end do
+
+  !print *, xl, xu !OK
+
+  print *, trim("True mean of the distribution: "), xm_true
+
+  print *, trim("Lower bound uncertainty: "), abs(xm_true-xl)
+  print *, trim("Upper bound uncertainty: "), abs(xm_true-xu)
 
 
 end program main
